148ac24d578547890db0d4fb612d8ab94a00c04f
afrasilv <alexfs92@gmail.com>
2017-11-25 21:04:13 +0100

    test onclicklisteners

--- app/src/main/java/com/example/afrasilv/exampleprojectkotlin/MainActivity.kt
+++ app/src/main/java/com/example/afrasilv/exampleprojectkotlin/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.afrasilv.exampleprojectkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +15,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        toast("hello")
-
+//        toast("hello")
 
         val tv = this.findViewById<TextView>(R.id.textView) as TextView //as para hacer casting
         tv.setText("Hello Kotlin")
         tv.text = "Hello Kotlin" //igual que lo de arriba
+        toast("hello $textView, how are you?")
+        toast("hello ${textView.text}, how are you?") //si es un objeto más complejo, tendríamos que usar los corchetes
+
+
+        val etv = findViewById<EditText>(R.id.message) as EditText
+        val button = findViewById<Button>(R.id.button) as Button
+
+        button.setOnClickListener{ toast("Hello ${etv.text}") }
     }
 
     //Unit = void en Java pero devolviendo un objeto (la función)
