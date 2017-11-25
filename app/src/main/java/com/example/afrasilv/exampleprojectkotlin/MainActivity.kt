70c9e8089d742874e7bc2398c0d6e72d579c3f5f
afrasilv <alexfs92@gmail.com>
2017-11-25 20:26:02 +0100

    test variables

--- app/src/main/java/com/example/afrasilv/exampleprojectkotlin/MainActivity.kt
+++ app/src/main/java/com/example/afrasilv/exampleprojectkotlin/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.afrasilv.exampleprojectkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,12 +12,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toast("hello")
+
+
+        val tv = this.findViewById<TextView>(R.id.textView) as TextView //as para hacer casting
+        tv.setText("Hello Kotlin")
+        tv.text = "Hello Kotlin" //igual que lo de arriba
     }
 
     //Unit = void en Java pero devolviendo un objeto (la función)
     private fun toast(message: String): Unit{
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
+
+
 
    /* fun getInt(int: Int): Int{
         return 2 * int
@@ -25,4 +34,14 @@ class MainActivity : AppCompatActivity() {
 
     fun getInt(int: Int) = 2 * int // infiere el tipo
     */
+
+    fun test() {
+        val x = 20
+        val y: Int = 20
+        var long: Long = x.toLong()
+        val string = "Hello"
+//        string = "goodbye" < -- al ser val es como final en java, no podemos cambiarle el valor, para ello tendríamos que usar var
+
+//        las variables en Kotlin son objetos podemos usar y.toString() por ejemplo
+    }
 }
